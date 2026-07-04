90bc7399b0474d37997bfbe15b07e83de421d519
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-04 09:11:14 +0500

    feat(files): implement secure upload handler with prisma and filesystem rollback on failure

---
+++ backend/controllers/fileController.js
@@ -0,0 +1,57 @@
+import { prisma } from "../config/prisma.js";
+import fs from 'fs';
+
+export const uploadFile = async (req, res) => {
+    let filePath;
+    try {
+        const file = req.file;
+        filePath = file?.path;
+
+        if (!file) {
+            return res.status(400).json({
+                success: false,
+                message: "Please upload a file",
+            })
+        }
+
+        const { category, documentType } = req.body;
+
+        if (!category || !documentType) {
+            return res.status(400).json({
+                success: false,
+                message: "category and documentType are required."
+            })
+        }
+
+        const uploadedFile = await prisma.file.create({
+            data: {
+                originalName: file.originalname,
+                filePath: file.path,
+                storedName: file.filename,
+                mimeType: file.mimetype,
+                size: file.size,
+
+                category,
+                documentType,
+
+                employeeId: req.user.id,
+                uploadedById: req.user.id,
+            }
+        })
+
+        return res.status(201).json({
+            success: true,
+            message: "File uploaded successfully",
+            file: uploadedFile,
+        })
+    } catch (error) {
+        if(filePath){
+            fs.unlink(filePath,()=>{});
+        }
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error,
+        })
+    }
+}
