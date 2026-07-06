fcbe92ce7e8f75a21967ad79c356e63ce5a89abb
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-06 12:13:19 +0500

    feat: add /api/v1/file route and integrate fileRoutes route handler

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 import authRoutes from './routes/authRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
-import { prisma } from './config/prisma.js';
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/file',fileRoutes)
+app.use('/api/v1/file', fileRoutes);
 app.listen(PORT, () => {
     console.log(`visit http://localhost:${PORT}`)
 })
