9c0d8f547fb46e4e4e39a0415f044855afd145d7
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-08 23:01:26 +0500

    feat: make destination directory of multer dynamic

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,8 +1,19 @@
 import multer from 'multer';
-
+import fs from 'fs/promises';
+import path from 'path';
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads/")
+    destination: async (req, file, cb) => {
+        try {
+            const { category } = req.body;
+            console.log(category);
+            const uploadPath = path.join(process.cwd(), "uploads", category);
+            await fs.mkdir(uploadPath, { recursive: true });
+
+            cb(null, uploadPath)
+        } catch (err) {
+            cb(err.message)
+        }
+
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
