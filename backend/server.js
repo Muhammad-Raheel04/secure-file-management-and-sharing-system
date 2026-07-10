3da0f8af3da8e39c377031eeeac89186173b50ea
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-10 22:12:53 +0500

    feat: integrate documment routes and add route for documents

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/authRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
+import documentRoutes from './routes/documentRoutes.js';
 const PORT = process.env.PORT;
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(express.json());
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/file', fileRoutes);
 app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/category',categoryRoutes)
+app.use('/api/v1/category',categoryRoutes);
+app.use('/api/v1/document',documentRoutes);
 app.listen(PORT, () => {
     console.log(`visit http://localhost:${PORT}`)
 })
