ea0e3554221891f13ef95dfdc898954cdef27385
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-10 19:10:43 +0500

    feat: add category controllers

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,77 @@
+import { prisma } from "../config/prisma.js";
+
+export const addCategory = async (req, res) => {
+    try {
+        const { category } = req.body;
+        if (!category) {
+            return res.status(400).json({
+                success: false,
+                message: "Reuired field is missing",
+            })
+        }
+        const exisitingCategory = await prisma.fileCategory.findUnique({
+            where: {
+                name: category
+            }
+        })
+        if (exisitingCategory) {
+            return res.status(409).json({
+                success: false,
+                message: "Category Already Exists"
+            })
+        }
+        await prisma.fileCategory.create({
+            data: {
+                name: category
+            }
+        })
+        return res.status(200).json({
+            success: true,
+            message: "Category Added Scucessfully",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error.message,
+        });
+    }
+}
+
+export const deleteCategory = async (req, res) => {
+    try {
+        const { category } = req.body;
+        if (!category) {
+            return res.status(400).json({
+                success: false,
+                message: "Reuired field is missing",
+            })
+        }
+        const exisitingCategory = await prisma.fileCategory.findUnique({
+            where: {
+                name: category
+            }
+        })
+        if (!exisitingCategory) {
+            return res.status(404).json({
+                success: false,
+                message: "Category doesn't Exists"
+            })
+        }
+        await prisma.fileCategory.delete({
+            where: {
+                name: category
+            }
+        })
+        return res.status(200).json({
+            success: true,
+            message: "Category deleted Scucessfully",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error.message,
+        })
+    }
+}
