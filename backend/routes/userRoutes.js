091303e4f51d5e399720f5b3f50ed53a5d589fa8
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-10 09:07:19 +0500

    feat: add patch route for updating user's roles

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
-import { addRole } from '../controllers/userController.js';
+import { addRole, assignRole } from '../controllers/userController.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 const router = express.Router();
 
-router.post("/add-role",isAuthenticated,addRole)
+router.post("/add-role", isAuthenticated, isAdmin, addRole)
+router.patch("/assign-role", isAuthenticated, isAdmin, assignRole);
 
 export default router;
