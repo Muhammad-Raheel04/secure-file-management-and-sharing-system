58a9d98d87d525965b7f08716ba98e68786713d9
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-10 16:29:38 +0500

    feat: add revoke-role route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
-import { addRole, assignRole } from '../controllers/userController.js';
+import { addRole, assignRole, removeRole, revokeRole } from '../controllers/userController.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 const router = express.Router();
 
-router.post("/add-role", isAuthenticated, isAdmin, addRole)
+router.post("/add-role", isAuthenticated, isAdmin, addRole);
+router.delete("/remove-role", isAuthenticated, isAdmin, removeRole);
 router.patch("/assign-role", isAuthenticated, isAdmin, assignRole);
-
+router.delete("/revoke-role", isAuthenticated, isAdmin, revokeRole)
 export default router;
