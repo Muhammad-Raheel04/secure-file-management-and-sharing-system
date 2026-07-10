a98ce8ab5584fb96ec37f3de7dd2fd7f8c48d85c
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-10 19:11:48 +0500

    feat: add endpoint for categories

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import authRoutes from './routes/authRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 const PORT = process.env.PORT;
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/file', fileRoutes);
 app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/category',categoryRoutes)
 app.listen(PORT, () => {
     console.log(`visit http://localhost:${PORT}`)
 })
