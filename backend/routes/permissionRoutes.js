3351cd816344ee1ea641af66b2d9500f24e07a68
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-11 10:11:16 +0500

    feat: add routes for granting and revoking permissions

--- backend/routes/permissionRoutes.js
+++ backend/routes/permissionRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
-import { addPermission, deletePermission } from '../controllers/permissionController.js';
+import { addPermission, deletePermission, grantRoleLevelPermissions, revokeRoleLevelPermissions } from '../controllers/permissionController.js';
 const router = express.Router();
 
 router.post("/add-permission", isAuthenticated, isAdmin, addPermission);
 router.delete("/delete-permission", isAuthenticated, isAdmin, deletePermission);
+router.post("/grant-role-permission",isAuthenticated,isAdmin,grantRoleLevelPermissions);
+router.delete('/revoke-role-permission',isAuthenticated,isAdmin,revokeRoleLevelPermissions);
 
 export default router;
