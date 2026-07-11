754f64100a2b8a247a22788ee71d81124df1c754
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-11 19:55:38 +0500

    remove deleted files

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -15,7 +15,7 @@ router.get("/:id/view", isAuthenticated, requireFileAccess("VIEW"), serveFile);
 router.patch("/:id", isAuthenticated, requireFileAccess("EDIT"), upload.single("file"), validateFileMetaData, updateFile);
 router.delete("/:id", isAuthenticated, requireFileAccess("DELETE"), deleteFile);
 
-router.post("/:id/grant-permissions", isAuthenticated, requirePermissionGrantAccess, grantFilePermission);
+router.post("/:id/grant-permissions", isAuthenticated, requirePermission("SHARE"), requireFileAccess("SHARE"),grantFilePermission);
 router.get("/:id/list-permissions", isAuthenticated, requirePermissionGrantAccess, listFilePermissions);
 router.delete("/:id/revoke-permissions/:userId", isAuthenticated, requirePermissionGrantAccess, revokeFilePermission);
 
--- backend/utils/isValidCategory.js
+++
@@ -1,5 +0,0 @@
-const validCategories = ["IDENTITY", "EDUCATION", "PROFESSIONAL", "FINANCIAL", "OTHER"];
-
-export const isValidCategory = (category) => {
-    return validCategories.includes(category);
-}
