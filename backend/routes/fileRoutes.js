4b72c8dd8d0502aee5c5b4f9605c6659e3840acd
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-11 16:24:13 +0500

    refactor: remove get route for getFile and update router handlers of serverFile router

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { deleteFile, getFile, grantFilePermission, listFilePermissions, revokeFilePermission, updateFile, uploadFile, serveFile } from "../controllers/fileController.js";
+import { deleteFile, grantFilePermission, listFilePermissions, revokeFilePermission, updateFile, uploadFile, serveFile } from "../controllers/fileController.js";
 import { upload } from "../middlewares/uploadMiddleware.js";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js";
 import { requireFileDeleteAccess, requireFileReadAccess, requireFileWriteAccess } from "../middlewares/fileAccess.js";
 import { requirePermissionGrantAccess } from "../middlewares/requirePermissionGrantAccess.js";
 import { requirePermission } from "../middlewares/requirePermission.js";
 import { validateFileMetaData } from "../middlewares/validateFileMetaData.js";
+import { requireFileAccess } from "../middlewares/requireFileAccess.js";
 
 const router = express.Router();
 
 router.post("/upload", isAuthenticated, requirePermission("UPLOAD"), upload.single("file"), validateFileMetaData, uploadFile);
-router.get("/:id", isAuthenticated, requireFileReadAccess, getFile);
-router.get("/:id/view", isAuthenticated, requireFileReadAccess, serveFile);
+router.get("/:id/view", isAuthenticated, requireFileAccess("READ"), serveFile);
 router.patch("/:id", isAuthenticated, requireFileWriteAccess, upload.single("file"), updateFile);
 router.delete("/:id", isAuthenticated, requireFileDeleteAccess, deleteFile);
 
