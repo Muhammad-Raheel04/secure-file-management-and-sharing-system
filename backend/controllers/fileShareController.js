b8c46aca58f4fce6cf1d68bb84c088898812b564
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-12 13:14:08 +0500

    feat: add controller to serve shared files

--- backend/controllers/fileShareController.js
+++ backend/controllers/fileShareController.js
@@ -1,5 +1,7 @@
 import crypto from 'crypto';
 import { prisma } from '../config/prisma.js';
+import path from 'path';
+import fs from 'fs';
 
 export const createFileShareLink = async (req, res) => {
     try {
@@ -28,7 +30,7 @@ export const createFileShareLink = async (req, res) => {
         return res.status(201).json({
             success: true,
             message: "Share Link created succcessfully",
-            shareUrl:`${process.env.BACKEND_API_URL}/share/${token}`,
+            shareUrl: `${process.env.BACKEND_API_URL}/${token}`,
             expiresAt,
         })
     } catch (error) {
@@ -37,5 +39,30 @@ export const createFileShareLink = async (req, res) => {
             message: "Internal Server Error",
             error: error.message,
         })
+    }
+}
+
+export const serveSharedFile = async (req, res) => {
+    try {
+        const file = req.file;
+
+        const absolutePath = path.resolve(file.filePath);
+
+        if (!fs.existsSync(absolutePath)) {
+            return res.status(404).json({
+                success: false,
+                message: "File not found.",
+            });
+        }
+
+        res.setHeader("Content-Type", file.mimeType);
+        res.setHeader("Content-Disposition", `inline; filename="${file.originalName}"`);
+        res.sendFile(absolutePath);
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error.message,
+        });
     }
 }
