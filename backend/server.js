89ce13f543043fffb500e8dd76d2525e34196a1f
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-18 13:33:29 +0500

    feat(server): register Excel worker

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import './cron/deletedExpiredFiles.js';
+import './workers/excelWorker.js';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import fileRoutes from './routes/fileRoutes.js';
