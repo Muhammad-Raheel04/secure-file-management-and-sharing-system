dcfaa5723db71d8d50da54d9ac3f1395ca18ba16
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-19 20:31:16 +0500

    feat(email): add excel processing failure email service

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,4 +1,5 @@
 import client from "../config/brevo.js";
+import excelProcessingFailureTemplate from "../templates/excelProcessingFailureTemplate.js";
 import excelProcessingSuccessTemplate from "../templates/excelProcessingSuccessTemplate.js";
 
 export const sendExcelProcessingSuccessEmail = async (name, email, fileName) => {
@@ -12,6 +13,24 @@ export const sendExcelProcessingSuccessEmail = async (name, email, fileName) => 
             subject: "File Processed Successfully",
             htmlContent: excelProcessingSuccessTemplate(name, fileName),
             params: { name, fileName },
+        })
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export const sendExcelProcessingFailureEmail = async (name, email, fileName, reason) => {
+    try {
+        await client.transactionalEmails.sendTransacEmail({
+            sender: {
+                name: process.env.BREVO_SENDER_NAME,
+                email: process.env.BREVO_SENDER_EMAIL,
+            },
+            to: [{ email, name }],
+            subject: "File Processed Successfully",
+            htmlContent: excelProcessingFailureTemplate(name, fileName,reason),
+            params: { name, fileName ,reason},
         })
     } catch (error) {
         console.error(error);
