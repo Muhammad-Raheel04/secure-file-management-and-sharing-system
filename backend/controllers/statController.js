8171997800964591904e5588ad284e06da36f70d
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-20 23:01:10 +0500

    feat: add rest of the required stats in response

--- backend/controllers/statController.js
+++ backend/controllers/statController.js
@@ -1,86 +1,57 @@
 import { prisma } from "../config/prisma.js"
-
-// this one is ok 
-// but below this one is more optimized
-// export const getStats = async (req, res) => {
-//     try {
-//         const [totalFiles, completed, failed, processing] = await Promise.all([
-//             prisma.workbook.aggregate({
-//                 _count: {
-//                     id: true
-//                 }
-//             }),
-//             prisma.workbook.aggregate({
-//                 _count: {
-//                     id: true,
-//                 },
-//                 where: {
-//                     status: 'COMPLETED'
-//                 }
-//             }),
-//             prisma.workbook.aggregate({
-//                 _count: {
-//                     id: true,
-//                 },
-//                 where: {
-//                     status: 'FAILED'
-//                 }
-//             }),
-//             prisma.workbook.aggregate({
-//                 _count: {
-//                     id: true,
-//                 },
-//                 where: {
-//                     status: 'PROCESSING'
-//                 }
-//             })
-//         ])
-
-//         return res.status(200).json({
-//             success: true,
-//             summary: {
-//                 totalFiles: totalFiles._count.id,
-//                 completed: completed._count.id,
-//                 failed: failed._count.id,
-//                 processing: processing._count.id,
-//                 successRate: totalFiles._count.id > 0 ? (completed._count.id / totalFiles._count.id) * 100 : 0
-//             }
-//         })
-//     } catch (error) {
-//         return res.status(500).json({
-//             success: false,
-//             message: "Internal Server Error",
-//             error: error.message,
-//         })
-//     }
-// }
+import excelQueue from '../queues/excelQueue.js';
 
 export const getStats = async (req, res) => {
     try {
-        const [totalFiles, statusCounts] = await Promise.all([
+        const [totalFiles, statusCounts, totalRecordsProcessed, totalWorkSheetsProcessed, completedWorkbooks, activeJobsCount, waitingJobsCount,failedJobsCount] = await Promise.all([
             prisma.workbook.count(),
             prisma.workbook.groupBy({
                 by: ['status'],
                 _count: {
                     status: true,
+                },
+            }),
+            prisma.worksheetRow.count(),
+            prisma.worksheet.count(),
+            prisma.workbook.findMany({
+                where: {
+                    status: 'COMPLETED'
+                },
+                select: {
+                    createdAt: true,
+                    updatedAt: true,
                 }
-            })
+            }),
+            await excelQueue.getActiveCount(),
+            await excelQueue.getWaitingCount(),
+            await excelQueue.getFailedCount(),
         ])
+
 
         const completed = statusCounts.find((item) => item.status === 'COMPLETED');
         const failed = statusCounts.find((item) => item.status === 'FAILED');
         const processing = statusCounts.find((item) => item.status === 'PROCESSING');
+        const totalProcessingTime = completedWorkbooks.reduce((total, workbook) => {
+            const processingTime = workbook.updatedAt - workbook.createdAt;
+            return total + processingTime;
+        }, 0);
 
         return res.status(200).json({
             success: true,
             message: "Stats fetched successfully",
             summary: {
                 totalFiles: totalFiles,
-                completed: completed?._count.status,
-                failed: failed?._count.status,
-                processing: processing?._count.status,
-                succesRate: totalFiles > 0 ? (completed?._count.status / totalFiles) * 100 : 0
-            }
+                completed: completed?._count.status || 0,
+                failed: failed?._count.status || 0,
+                processing: processing?._count.status || 0,
+                succesRate: totalFiles > 0 ? (completed?._count.status / totalFiles) * 100 : 0,
+                totalRecordsProcessed,
+                totalWorkSheetsProcessed,
+                avgProcessingTime: completedWorkbooks.length > 0 ? (totalProcessingTime / completedWorkbooks.length) / 1000 : 0,
+                waitingJobsCount,
+                activeJobsCount,
+                failedJobsCount,
+            },
         })
     } catch (error) {
         return res.status(500).json({
