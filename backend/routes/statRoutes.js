59c9708b6300932a3b0e8c19b6ccf3ac93217490
Muhammad Raheel <raheel31102004@gmail.com>
2026-07-21 19:47:35 +0500

    feat: register cache middleware

--- backend/routes/statRoutes.js
+++ backend/routes/statRoutes.js
@@ -2,8 +2,9 @@ import express from 'express';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { getStats } from '../controllers/statController.js';
+import { cache } from '../middlewares/cache.js';
 const router = express.Router();
 
-router.get('/', isAuthenticated, isAdmin, getStats);
+router.get('/', isAuthenticated, isAdmin, cache("stats:dashboard", 300), getStats);
 
 export default router;
